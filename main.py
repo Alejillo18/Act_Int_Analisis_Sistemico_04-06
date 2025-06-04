22faf13a7d312a64867815deb122328a719848e7
Alejo <Alejoemmanueloviedo@gmail.com>
2025-06-04 19:44:51 -0300

    Imp_Operaciones. Closes #7

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import operaciones
 def main():
     print(operaciones.suma(1,2))
     print(operaciones.resta(-3,2))
-
+    print(operaciones.division(3,2))
+    print(operaciones.producto(3,1))
 if __name__ == "__main__":
     main()
